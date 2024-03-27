7abef0d90fe7200597c7cab612a1572fa3182775
ComandanteTK <descontraindolive@gmail.com>
2024-03-27 20:43:44 -0300

    Adding TypeScript functions: show menu, hidden menu and remove mobile menu / Adicionando funções TypeScript: menu show, menu hidden e remove menu mobile

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 "use strict";
-const nam = "nathan";
-const numero = 15;
-console.log(numero);
+//=================== SHOW MENU ====================
+const navMenu = document.getElementById("nav-menu"), navToggle = document.getElementById("nav-toggle"), navClose = document.getElementById("nav__close");
+//========== MENU SHOW ===========
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        if (navMenu) {
+            navMenu.classList.add("show-menu");
+        }
+    });
+}
+//========= MENU HIDDEN ==========
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        if (navMenu) {
+            navMenu.classList.remove("show-menu");
+        }
+    });
+}
+//====================== REMOVE MENU MOBILE =================
+const navLink = document.querySelectorAll(".nav__link");
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    if (navMenu) {
+        navMenu.classList.remove("show-menu");
+    }
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+// =================================================================
